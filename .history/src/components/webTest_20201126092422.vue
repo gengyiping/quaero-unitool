33fc5eb2c068e05a30b3f95c7b5867cc7c975a58
zhangbinbtx <771566497@qq.com>
2020-11-26 14:06:11 +0800

    刷新后自动跳转上一次所在页面

---
+++ .history/src/components/webTest_20201126092422.vue
@@ -0,0 +1,470 @@
+<template>
+  <div class="" > 
+    <el-dialog
+  title="提示"
+  :visible.sync="centerDialogVisible"
+  width="30%"
+  center>
+  <span v-html="coord"></span>
+  <span slot="footer" class="dialog-footer">
+       <el-button @click="centerDialogVisible = false" >取 消</el-button>
+  </span>
+</el-dialog>
+     <div style="width:100%;background:#40E0D0">
+       <span style="font-size: 38px;color: floralwhite;font-family: 'Helvetica Neue',Helvetica,'PingFang SC','Hiragino Sans GB','Microsoft YaHei','微软雅黑',Arial,sans-serif;">
+         简易Qcit
+         </span>
+       <i class="el-icon-video-play" @click="init" style="font-size: 45px; color: #7FFF00" v-if="initshow"></i>
+       <i class="el-icon-video-pause" style="font-size: 45px; color: #DC143C" v-if="closeshow"></i>
+      <!--  <el-button @click="init">初始化</el-button>
+         <el-button @click="allReset">整机复位</el-button>
+        <el-button @click="senMessage">测试发送消息</el-button> -->
+    </div>
+     <div class="tabBox" >
+        <el-tabs :tab-position="tabPosition" v-model="activeName" @tab-click="handleClick" style="height: 708px;">
+            <el-tab-pane label="应用" name="0">
+               <Qcitsoft height="100%" width="100%" />
+            </el-tab-pane>
+            <el-tab-pane label="调试" name="1">
+               <Qcitdebug height="100%" width="100%"/>
+            </el-tab-pane>
+            <el-tab-pane label="Demo" name="2">
+                <Demo height="10000px" width="100%"/>
+            </el-tab-pane>
+        </el-tabs>
+        
+     </div>
+     <div>
+       <el-divider content-position="left"></el-divider>
+          <el-row :gutter="20" style="">
+             <el-col :span="16"><div class="grid-content bg-purple"></div></el-col>
+             <el-col :span="8"><div class="grid-content bg-purple">版本信息</div></el-col>
+          </el-row>
+     </div>
+     <div id='ip' ></div>
+     <h1>{{ ip }}</h1>
+  </div>
+  
+</template>
+<script>
+import SockJS from 'sockjs-client'
+import Stomp from 'webstomp-client'
+import Qcitdebug from '@/view/debugger/Qcitdebug'
+import Qcitsoft from '@/view/soft/Qcitsoft'
+import Demo from '@/view/demo/divMove'
+import Bus from './bus'
+import {getNetworkIp} from './ip'
+export default {
+  data() {
+    return {
+      tabPosition: 'left',
+      list1: [],
+      stompClient: null,
+      initshow:true,
+      closeshow:false,
+      allmotor:null,
+       centerDialogVisible: false,
+       coord:null,
+       ip:'',
+       activeName: '0',
+       pagestate:'',
+       socketUrl: 'http://127.0.0.1:8080/webSocket',
+       pageSate:''
+
+    }
+  },
+  components: {
+    Qcitdebug,
+    Qcitsoft,
+    Demo
+    },
+  mounted() {
+   //  this.ready() //外网ip
+    this.initWebSocket()
+     this.getUserIP()
+    
+    this.$store.state.stompClient=this.stompClient
+      //this.ip=this.$store.state.ip
+  },
+  methods: {
+    init(){
+      debugger
+       console.log('init')
+      this.stompClient.send('/app/init')
+      this.initshow=false
+      this.closeshow=true
+    },
+    initstate(){
+      debugger
+    //  this.ip=document.getElementById("iploc").defaultValue
+       console.log('initstate'+this.ip)
+       debugger
+        console.log('initstate'+localStorage.getItem('ip_addr'))
+       
+       this.stompClient.send('/app/initstate/'+this.ip,{})
+    },
+     ready:function(){
+                //获取本地IP地址
+                //js 引入 <!-- 获取本机ip  -->
+                //<script src="http://pv.sohu.com/cityjson?ie=utf-8">
+            var cip = returnCitySN["cip"];
+        },
+      getUserIP(){
+      var RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
+      if (RTCPeerConnection) (()=>{
+      var rtc = new RTCPeerConnection()
+      rtc.createDataChannel(''); //创建一个可以发送任意数据的数据通道
+      rtc.createOffer( offerDesc => { //创建并存储一个sdp数据 
+      rtc.setLocalDescription(offerDesc)
+       }, e => { console.log(e)})
+       rtc.onicecandidate =(evt) => { //监听candidate事件
+       if (evt.candidate) {
+        var ip_addr = evt.candidate.address
+         //let ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g
+         //var ips=ipRegex.exec(evt.candidate.candidate)
+        localStorage.setItem('ip_addr',ip_addr)
+        var xip=localStorage.getItem('ip_addr')
+       // this.$store.state.ip=ip_addr
+       this.ip=ip_addr
+        console.log('ip='+this.ip)
+         var _this = this;// `这一步很重要`
+        setTimeout(function() { //防止报连接yet
+          _this.initstate()//获取上一次连接状态
+        }, 2000);
+        console.log("目前仅测试了chrome浏览器OK"+ip_addr)
+       }}
+        })()
+      else{console.log("目前仅测试了chrome浏览器OK")}
+     },
+      handleClick(tab, event) {
+        debugger
+        console.log(tab, event);
+        var page='0_'+tab.name+'_#_#'
+        this.stompClient.send('/app/page/'+this.ip+'/'+page,{})
+      },
+       firstPage(){
+        return  this.$store.state.firstPage;
+       },
+    // allReset(){
+    //    console.log('allReset')
+    //   this.stompClient.send('/app/allreset')
+    // },
+    // senMessage() {
+    //   console.log('senMessage')
+    //   this.stompClient.send('/app/test')
+    // },
+    initWebSocket() {
+       console.log('initWebSocket')
+    //  this.connection()
+      // 需要有一个失败重连得到问题
+      this.initSocket()
+    },
+    initSocket() {
+      this.socket = new SockJS(this.socketUrl)// 连接服务端
+      this.stompClient = Stomp.over(this.socket)
+      this.stompClient.connect({}, (frame) => {
+        this.successCallback()
+      }, () => {
+        this.reconnect(this.socketUrl, this.successCallback)
+      })
+    },
+    // 断开重连使用定时器定时连接服务器
+    reconnect(socketUrl, successCallback) {
+      console.info('in reconnect function')
+      let connected = false
+      const reconInv = setInterval(() => {
+        console.info('in interval' + Math.random())
+        this.socket = new SockJS(socketUrl)
+        this.stompClient = Stomp.over(this.socket)
+        this.stompClient.connect({}, (frame) => {
+          console.info('reconnected success')
+          // 连接成功，清除定时器
+          clearInterval(reconInv)
+          connected = true
+          successCallback()
+        }, () => {
+          console.info('reconnect failed')
+          console.info('connected:' + connected)
+          if (connected) {
+            console.info('connect .... what?')
+          }
+        })
+      }, 2000)
+    },
+    // 接收到消息并对消息做处理
+    onMessageReceived(payload) {
+      var message = JSON.parse(payload.body)
+      console.info('Message', message)
+    },
+    // 连接成功
+    successCallback() {
+       var _this = this;// `这一步很重要`
+      console.info('onConnected')
+      // this.stompClient.subscribe('/topic/topicid', this.onMessageReceived)
+      // this.stompClient.send('/app/msg',
+      //   {},
+      //   JSON.stringify({ sender: 'sender', type: 'JOIN' })
+      // )
+        this.stompClient.subscribe('/topic/getResponse', (val) => {
+          // this.list1 = JSON.parse(val.body)
+          console.log('-------++++++++++++++++++++++++++++++------------')
+//下面会报错，应该是json的问题，但是数据可以接收到
+          console.log(val.body)
+        });
+        this.stompClient.subscribe('/user/initstate/alone/getResponse', (val) => { //获取上一次页面状态
+          // this.list1 = JSON.parse(val.body)
+          debugger
+          console.log(val.body)
+          if(this.pagestate!=val.body){
+              var pagesindexs=val.body.split("_")
+              this.$store.state.rootPage=val.body,
+              this.$store.state.firstPage=val.body,
+              // this.$store.state.secondPage=pagesindexs[2],
+              // this.$store.state.thirdPage=pagesindexs[3]
+          }
+        });
+        this.stompClient.subscribe('/user/page/alone/getResponse', (val) => {
+          // this.list1 = JSON.parse(val.body)
+          debugger
+          console.log(val.body)
+        });
+        
+         this.stompClient.subscribe('/user/allmotor/alone/getResponse', function (res) {
+            console.log("----allmotor----")
+            debugger
+            _this.allmotor = res.body
+            Bus.$emit('allmotor',_this.allmotor)
+             console.log("----allmotor----"+_this.allmotor)
+        });
+        
+         this.stompClient.subscribe('/user/chip/alone/getResponse', function (res) {//获取芯片ID
+              _this.allmotor = res.body
+              Bus.$emit('chipID',_this.allmotor)
+             console.log("----allmotor----"+_this.allmotor)
+        });
+         this.stompClient.subscribe('/user/openTftp/alone/getResponse', function (res) {//开关tftp
+            _this.allmotor = res.body
+            Bus.$emit('opentftp',_this.allmotor)
+             console.log("----allmotor----"+_this.allmotor)
+        });
+         this.stompClient.subscribe('/user/openRemote/alone/getResponse', function (res) {//开关远程服务
+            _this.allmotor = res.body
+            _this.coord= _this.allmotor;
+            _this.centerDialogVisible =true
+        });
+         this.stompClient.subscribe('/user/sensorSearch/alone/getResponse', function (res) {//查询所有传感器
+            _this.allmotor = res.body
+            Bus.$emit('allsensor',_this.allmotor)
+             console.log("----allmotor----"+_this.allmotor)
+        });
+         this.stompClient.subscribe('/user/readCoord/alone/getResponse', function (res) {//读取定标参数
+         debugger
+            _this.allmotor = res.body
+            Bus.$emit('readCoord',_this.allmotor)
+             console.log("----allmotor----"+_this.allmotor)
+        });
+         this.stompClient.subscribe('/user/writeCoord/alone/getResponse', function (res) {//保存定标参数
+            _this.allmotor = res.body
+             _this.coord= _this.allmotor;
+            _this.centerDialogVisible =true
+        });
+        this.stompClient.subscribe('/user/searchMotor/alone/getResponse', function (res) {//查询所有电机信息
+        debugger
+            _this.allmotor = res.body
+            Bus.$emit('searchMotor',_this.allmotor)
+             console.log("----allmotor----"+_this.allmotor)
+        });
+         this.stompClient.subscribe('/user/motorOpt/alone/getResponse', function (res) {//查询所有电机信息控制
+            debugger
+            _this.allmotor = res.body
+            _this.coord= _this.allmotor
+              _this.centerDialogVisible =true
+        });
+         this.stompClient.subscribe('/user/transferMove/alone/getResponse', function (res) {//区域转移
+            debugger
+             _this.allmotor = res.body
+              _this.coord= _this.allmotor
+              _this.centerDialogVisible =true
+        });
+         this.stompClient.subscribe('/user/firupload/alone/getResponse', function (res) {//监听上传文件
+            debugger
+             _this.allmotor = res.body
+             Bus.$emit('firupload',res.body)
+        });
+      
+
+         this.stompClient.subscribe('/user/motorcontrol/alone/getResponse', function (res) {
+            console.log("----motorcontrol----")
+            _this.allmotor = res.body
+            // Bus.$emit('allmotor',_this.allmotor);
+            // console.log("----allmotor----"+_this.allmotor);
+        });
+        this.stompClient.subscribe('/user/getCoord/alone/getResponse', function (res) {
+          debugger;
+            console.log("----getCoord----")
+            _this.coord="当前坐标："+JSON.parse(res.body)
+            _this.centerDialogVisible =true
+        });
+        this.stompClient.subscribe('/user/steplost/alone/getResponse', function (res) {
+          debugger;
+            console.log("----steplost----");
+          let reg=new RegExp(';','g')//g代表全部
+                let newMsg=res.body.replace(reg,'<br/>')
+                 console.log("----steplost----"+newMsg)
+                 _this.coord=newMsg;
+            _this.centerDialogVisible =true
+        });
+        this.stompClient.subscribe('/user/motorFlag/alone/getResponse', function (res) {
+          debugger;
+            console.log("----motorFlag----");
+             let reg=new RegExp(';','g')//g代表全部
+                let newMsg=res.body.replace(reg,'<br/>')
+                 console.log("----steplost----"+newMsg)
+                 _this.coord=newMsg
+            _this.centerDialogVisible =true
+        });
+        this.stompClient.subscribe('/user/sensorStop/alone/getResponse', function (res) {
+          debugger;
+            console.log("----sensorStop----")
+                 _this.coord=res.body
+            _this.centerDialogVisible =true
+        });
+         this.stompClient.subscribe('/user/grooveMove/alone/getResponse', function (res) {
+          debugger;
+            console.log("----grooveMove----")
+                 _this.coord=res.body
+                 _this.centerDialogVisible =true
+        });
+         this.stompClient.subscribe('/user/grooveMove/alone/getResponse', function (res) {
+          debugger;
+            console.log("----grooveMove----")
+                 _this.coord=res.body
+                 _this.centerDialogVisible =true
+        });
+        this.stompClient.subscribe('/user/searchInlineopt/alone/getResponse', function (res) {
+          debugger;
+            console.log("----searchInlineopt----")
+                 _this.coord=res.body
+                  Bus.$emit('searchInlineopt',_this.coord)
+               //  _this.centerDialogVisible =true
+        });
+         this.stompClient.subscribe('/user/writeInlineopt/alone/getResponse', function (res) {
+          debugger;
+            console.log("----writeInlineopt----")
+                 _this.coord=res.body
+                 _this.centerDialogVisible =true
+        });
+        this.stompClient.subscribe('/user/coordLimit/alone/getResponse', function (res) {
+          debugger;
+            console.log("----coordLimit----")
+                var op= res.body.split(';')
+                if(op[2]=='read'){
+                    Bus.$emit('phymax',res.body)
+                }else{
+                  _this.coord=res.body
+                  _this.centerDialogVisible =true
+                }
+        });
+         this.stompClient.subscribe('/user/outStepForm/alone/getResponse', function (res) {
+          debugger;
+            console.log("----outStepForm----")
+                var op= res.body.split(';')
+                if(op[2]=='read'){
+                    Bus.$emit('outMove',res.body)
+                }else{
+                  _this.coord=res.body
+                  _this.centerDialogVisible =true
+                }
+        });
+        this.stompClient.subscribe('/user/zeroOpt/alone/getResponse', function (res) {
+          debugger;
+            console.log("----zeroOpt----")
+                var op= res.body.split(';')
+                if(op[1]=='read'){
+                    Bus.$emit('zeroOpt',res.body)
+                }else{
+                  _this.coord=res.body
+                  _this.centerDialogVisible =true
+                }
+        });
+        this.stompClient.subscribe('/user/createMotorFile/alone/getResponse', function (res) {
+          debugger;
+            console.log("----createMotorFile----")
+                  _this.coord=res.body
+                  _this.centerDialogVisible =true
+        });
+         this.stompClient.subscribe('/user/speedAcc/alone/getResponse', function (res) {
+          debugger;
+            console.log("----speedAcc----")
+            var op= res.body.split(';')
+              if(op[4]=='read'){
+                    Bus.$emit('speedAccList',res.body)
+                }else{
+                  _this.coord=res.body
+                  _this.centerDialogVisible =true
+                }
+        });
+         this.stompClient.subscribe('/user/upload/alone/getResponse', function (res) {
+          debugger;
+                  _this.coord=res.body
+                  _this.centerDialogVisible =true
+        });
+    },
+  },
+  watch:{
+    firstPage(newVal,oldVal){
+      debugger
+       // this.activeName=pagesindexs[1]
+       //do
+    },
+},
+}
+</script>
+<style rel="stylesheet/scss" lang="scss">
+.tabBox{
+  margin-top: 20px;
+  .el-tabs--left{
+    height: auto !important;
+    .el-tabs__item{
+      width: 60px;
+      line-height: 30px;
+      height: auto;
+      word-wrap: break-word;
+      white-space: pre-line;
+      margin-bottom: 25px;
+    };
+  
+  };
+};
+.el-tab-pane{
+margin-top:0px !important;
+}
+.el-row {
+    margin-bottom: 20px;
+    margin-top: -23px;
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+  .bg-purple-dark {
+    background: #99a9bf;
+  }
+  .bg-purple {
+    background: #d3dce6;
+  }
+  .bg-purple-light {
+    background: #e5e9f2;
+  }
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
+  .row-bg {
+    padding: 10px 0;
+    background-color: #f9fafc;
+  }
+
+</style>
---
+++ .history/src/view/debugger/Qcitdebug_20201126110220.vue
@@ -0,0 +1,116 @@
+<template>
+<div class="width:100%;height:100%">
+<el-menu
+:default-active="activeIndex"
+  class="el-menu-demo"
+  mode="horizontal"
+  @select="handleSelect"
+  background-color="#545c64"
+  text-color="#fff"
+  active-text-color="#ffd04b"
+  >
+  <el-menu-item index="1">OverView</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">进样器+前置仓</template>
+    <el-menu-item index="2-1">基本操作</el-menu-item>
+    <el-menu-item index="2-2">定标</el-menu-item>
+  </el-submenu>
+   <el-submenu index="3" >
+     <template slot="title">电机</template>
+    <el-menu-item index="3-1">基本操作</el-menu-item>
+    <el-menu-item index="3-2">更多操作</el-menu-item>
+   </el-submenu>
+   <el-menu-item index="4">测试</el-menu-item>
+</el-menu>
+  <div id="maincontain" style="width:100%;height:100%;background: #e5e9f2;">
+<keep-alive>
+    <OVERVIEW v-if="index===1"></OVERVIEW>
+    <INJECTION v-if="index===2"></INJECTION>
+     <INJECTIONCALIB v-if="index===3"></INJECTIONCALIB>
+    <MOTOR v-else-if="index===4" ></MOTOR>
+    <MOTORMORE v-else-if="index===5"></MOTORMORE>
+    <TEST v-else-if="index===6"></TEST>
+    <OVERVIEW v-else></OVERVIEW>  
+  </keep-alive>
+
+</div>
+</div>
+</template>
+<script>
+import SockJS from 'sockjs-client'
+import Stomp from 'webstomp-client'
+import MOTORBASE from '@/view/debugger/motor'
+import MOREMOTOR from '@/view/debugger/motormore'
+import OverView from '@/view/debugger/OverView'
+import InjectionCalib from '@/view/debugger/InjectionCalib'
+import Injection from '@/view/debugger/injection'
+import Test from '@/view/debugger/Test'
+
+export default {
+  data() {
+    return {
+      list1: [],
+      stompClient: null,
+      activeIndex: '1',
+       activeIndex2: '1',
+      index:0,
+    }
+  },
+   components:{
+         OVERVIEW:OverView,
+         INJECTION:Injection,
+         INJECTIONCALIB:InjectionCalib,
+         MOTOR:MOTORBASE,
+         MOTORMORE:MOREMOTOR,
+         TEST:Test
+   },
+  mounted() {
+   this.stompClient= this.$store.state.stompClient;
+  },
+  methods: {
+ handleSelect(key, keyPath) {
+   var _this=this
+     debugger
+       var indexkey=0;
+        console.log(key, keyPath);
+        if(key=="1")indexkey=1;
+        if(key=="2-1")indexkey=2;
+         if(key=="2-2")indexkey=3;
+        if(key=="3-1")indexkey=4;
+        if(key=="3-2")indexkey=5;
+         if(key=="4")indexkey=6;
+        this.index =indexkey;
+        debugger
+       var pages=this.$store.state.rootPage.split("_")
+       if(this.index!=pages[2]){
+        var page=pages[0]+'_'+pages[1]+'_'+this.index+'_#'
+        var ip=localStorage.getItem('ip_addr')
+        _this.stompClient.send('/app/page/'+ip+'/'+page,{})
+       }
+      }
+  },
+   computed: {
+    secondPage(){
+         debugger
+        return  this.$store.state.secondPage
+       },  
+  },
+  watch:{
+    firstPage(newVal,oldVal){
+      debugger
+        console.log("newVal="+newVal)
+         console.log("oldVal="+oldVal)
+             if(newVal!==oldVal){
+          var pagesindexs=newVal.split("_")
+           this.index =pagesindexs[2]
+          }
+           this.$store.state.thirdPage=newVal
+    },
+  },
+}
+
+</script>
+<style rel="stylesheet/scss" lang="scss">
+
+
+</style>
