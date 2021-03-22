e95011434256d2e9781eb50a0a595ed506a44b20
zhangbinbtx <771566497@qq.com>
2021-03-22 17:57:20 +0800

    删除不必要的文件

--- src/main/java/com/small/qcit/SmallQcitApplication.java
+++ src/main/java/com/small/qcit/SmallQcitApplication.java
@@ -2,34 +2,35 @@ package com.small.qcit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-import com.small.qcit.test.SomeMethod;
-import com.small.qcit.test.SomeThread;
-
+//@ComponentScan("com.quaero.base")
 @SpringBootApplication
+@EnableAsync
 public class SmallQcitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmallQcitApplication.class, args);
-		SomeMethod m = new SomeMethod();
-		SomeThread t = new SomeThread(m);
-		t.start();
-		new Thread() {
-			public void run() {
-				while(true){
-					m.m1();
-				}
-			}
-		}.start();	
-		while (true) {
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("Current method " + m.getCurrentMethodName());
-		}
+//		SomeMethod m = new SomeMethod();
+//		SomeThread t = new SomeThread(m);
+//		t.start();
+//		new Thread() {
+//			public void run() {
+//				while(true){
+//					m.m1();
+//				}
+//			}
+//		}.start();	
+//		while (true) {
+//			try {
+//				Thread.sleep(5000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			System.out.println("Current method " + m.getCurrentMethodName());
+//		}
 	}
 
 }
